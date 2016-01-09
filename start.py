896cd606f456f403755650a37f2b685845ef0e4c
lixianyang <705834854@qq.com>
2016-01-09 14:46:51 +0800

    release 1.0, crawl all comics

--- start.py
+++ start.py
@@ -8,10 +8,18 @@ from TouTiaoBaiJia.utils import append_start_url
 
 
 def start_comic():
-    start_urls = map(g_start_url, [STATUS["complete"]]*3, CATEGORY.values(), [1]*3)
-    # for url in start_urls:
-    #     append_start_url(url, COMIC_URLS_QUEUE)
-    append_start_url(start_urls[0], COMIC_URLS_QUEUE)
+    start_urls = list()
+    juvenile = CATEGORY["juvenile"]
+    maiden = CATEGORY["maiden"]
+    youth = CATEGORY["youth"]
+    for page in range(1, 375):
+        start_urls.append(g_start_url(STATUS["complete"], juvenile, page))
+    for page in range(1, 123):
+        start_urls.append(g_start_url(STATUS["complete"], maiden, page))
+    for page in range(1, 22):
+        start_urls.append(g_start_url(STATUS["complete"], youth, page))
+    for url in start_urls:
+        append_start_url(url, COMIC_URLS_QUEUE)
 
 
 if __name__ == '__main__':
