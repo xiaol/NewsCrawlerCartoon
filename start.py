c9bbf01d5bbbc9fa34d2b722f280ffce0561adf3
lixianyang <705834854@qq.com>
2016-01-14 16:07:58 +0800

    only crawl first page in comics

--- start.py
+++ start.py
@@ -12,12 +12,15 @@ def start_comic():
     juvenile = CATEGORY["juvenile"]
     maiden = CATEGORY["maiden"]
     youth = CATEGORY["youth"]
-    for page in range(1, 375):
-        start_urls.append(g_start_url(STATUS["complete"], juvenile, page))
-    for page in range(1, 123):
-        start_urls.append(g_start_url(STATUS["complete"], maiden, page))
-    for page in range(1, 22):
-        start_urls.append(g_start_url(STATUS["complete"], youth, page))
+    # for page in range(1, 375):
+    #     start_urls.append(g_start_url(STATUS["complete"], juvenile, page))
+    # for page in range(1, 123):
+    #     start_urls.append(g_start_url(STATUS["complete"], maiden, page))
+    # for page in range(1, 22):
+    #     start_urls.append(g_start_url(STATUS["complete"], youth, page))
+    start_urls.append(g_start_url(STATUS["complete"], juvenile, 1))
+    start_urls.append(g_start_url(STATUS["complete"], maiden, 1))
+    start_urls.append(g_start_url(STATUS["complete"], youth, 1))
     for url in start_urls:
         append_start_url(url, COMIC_URLS_QUEUE)
 
